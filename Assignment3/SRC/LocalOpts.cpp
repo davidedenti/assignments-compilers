794e16c567bb4f1485568b0c12c8c28550b729b8
davidedenti <davidedenti04@gmail.com>
2026-08-18 11:27:52 +0200

    Implementa LICM: individuazione istruzioni invarianti e spostamento nel preheader

--- Assignment3/SRC/LocalOpts.cpp
+++ Assignment3/SRC/LocalOpts.cpp
@@ -21,7 +21,9 @@ namespace {
 bool isReachingDefinitionFunction(Instruction &I, Loop &L, std::set<Instruction *> &IstruzioniInvarianti) {
   BinaryOperator *Op = dyn_cast<BinaryOperator>(&I);
 
-  if (!Op) { return false; }
+  if (!Op) {
+    return false;
+  }
 
   Value *Op1 = Op->getOperand(0);
   Value *Op2 = Op->getOperand(1);
@@ -32,10 +34,84 @@ bool isReachingDefinitionFunction(Instruction &I, Loop &L, std::set<Instruction 
   bool Definizione1DentroLoop = Definizione1 && L.contains(Definizione1->getParent());
   bool Definizione2DentroLoop = Definizione2 && L.contains(Definizione2->getParent());
 
-  if (Definizione1DentroLoop && IstruzioniInvarianti.find(Definizione1) == IstruzioniInvarianti.end()) { return false; }
-  if (Definizione2DentroLoop && IstruzioniInvarianti.find(Definizione2) == IstruzioniInvarianti.end()) { return false; }
+  if (Definizione1DentroLoop && IstruzioniInvarianti.find(Definizione1) == IstruzioniInvarianti.end()) {
+    return false;
+  }
+  if (Definizione2DentroLoop && IstruzioniInvarianti.find(Definizione2) == IstruzioniInvarianti.end()) {
+    return false;
+  }
 
   return true;
+}
+
+bool processLoop(Loop &L, DominatorTree &DT) {
+  bool IRModificato = false;
+
+  // Prima ricorsione sui sub-loop: processiamo prima i loop più interni,
+  // così eventuale codice che diventa invariant "a cascata" verso l'esterno
+  // può essere gestito correttamente quando arriviamo al loop corrente.
+  for (Loop *SottoLoop : L.getSubLoops()) {
+    if (processLoop(*SottoLoop, DT)) {
+      IRModificato = true;
+    }
+  }
+
+  std::set<Instruction *> IstruzioniInvarianti;
+  std::vector<Instruction *> IstruzioniInvariantiInOrdine;
+  bool NuovaInvariante = false;
+
+  do {
+    NuovaInvariante = false;
+
+    for (BasicBlock *BB : L.blocks()) {
+      for (Instruction &I : *BB) {
+        if (isReachingDefinitionFunction(I, L, IstruzioniInvarianti)) {
+          auto Inserimento = IstruzioniInvarianti.insert(&I);
+
+          if (Inserimento.second) {
+            NuovaInvariante = true;
+            IstruzioniInvariantiInOrdine.push_back(&I);
+
+            outs() << "Istruzione potenzialmente loop-invariant: ";
+            I.print(outs());
+            outs() << "\n";
+          }
+        }
+      }
+    }
+  } while (NuovaInvariante);
+
+  BasicBlock *Preheader = L.getLoopPreheader();
+
+  if (!Preheader) {
+    return IRModificato;
+  }
+
+  SmallVector<BasicBlock *, 8> BlocchiUscita;
+  L.getExitBlocks(BlocchiUscita);
+
+  for (Instruction *I : IstruzioniInvariantiInOrdine) {
+    BasicBlock *BloccoIstruzione = I->getParent();
+    bool DominaTutteLeUscite = true;
+
+    for (BasicBlock *BloccoUscita : BlocchiUscita) {
+      if (!DT.dominates(BloccoIstruzione, BloccoUscita)) {
+        DominaTutteLeUscite = false;
+        break;
+      }
+    }
+
+    if (DominaTutteLeUscite) {
+      outs() << "Sposto nel preheader: ";
+      I->print(outs());
+      outs() << "\n";
+
+      I->moveBefore(Preheader->getTerminator());
+      IRModificato = true;
+    }
+  }
+
+  return IRModificato;
 }
 
 struct LoopInvariantMotion : PassInfoMixin<LoopInvariantMotion> {
@@ -45,61 +121,14 @@ struct LoopInvariantMotion : PassInfoMixin<LoopInvariantMotion> {
     bool IRModificato = false;
 
     for (Loop *L : LI) {
-      std::set<Instruction *> IstruzioniInvarianti;
-      std::vector<Instruction *> IstruzioniInvariantiInOrdine;
-      bool NuovaInvariante = false;
-
-      do {
-        NuovaInvariante = false;
-
-        for (BasicBlock *BB : L->blocks()) {
-          for (Instruction &I : *BB) {
-            if (isReachingDefinitionFunction(I, *L, IstruzioniInvarianti)) {
-              auto Inserimento = IstruzioniInvarianti.insert(&I);
-
-              if (Inserimento.second) {
-                NuovaInvariante = true;
-                IstruzioniInvariantiInOrdine.push_back(&I);
-
-                outs() << "Istruzione potenzialmente loop-invariant: ";
-                I.print(outs());
-                outs() << "\n";
-              }
-            }
-          }
-        }
-      } while (NuovaInvariante);
-
-      BasicBlock *Preheader = L->getLoopPreheader();
-
-      if (!Preheader) { continue; }
-
-      std::vector<BasicBlock *> BlocchiUscita;
-      L->getExitBlocks(BlocchiUscita);
-
-      for (Instruction *I : IstruzioniInvariantiInOrdine) {
-        BasicBlock *BloccoIstruzione = I->getParent();
-        bool DominaTutteLeUscite = true;
-
-        for (BasicBlock *BloccoUscita : BlocchiUscita) {
-          if (!DT.dominates(BloccoIstruzione, BloccoUscita)) {
-            DominaTutteLeUscite = false;
-            break;
-          }
-        }
-
-        if (DominaTutteLeUscite) {
-          outs() << "Sposto nel preheader: ";
-          I->print(outs());
-          outs() << "\n";
-
-          I->moveBefore(Preheader->getTerminator());
-          IRModificato = true;
-        }
+      if (processLoop(*L, DT)) {
+        IRModificato = true;
       }
     }
 
-    if (IRModificato) { return PreservedAnalyses::none(); }
+    if (IRModificato) {
+      return PreservedAnalyses::none();
+    }
 
     return PreservedAnalyses::all();
   }
